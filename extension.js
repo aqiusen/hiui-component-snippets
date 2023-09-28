4034e3c1f6236db4cdd7f71402e0891f1239cc0d
qiusen <v-qiusen@xiaomi.com>
2023-09-28 17:02:07 +0800

    增加button 和 table 的提示

--- extension.js
+++ extension.js
@@ -19,12 +19,21 @@ function activate(context) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('hiui-component-snippets.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
-
+		debugger
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from hiui-component-snippets!');
 	});
+	//监听用户是否执行了这个命令：
+	const tableSnip = vscode.workspace.onDidExecuteTextEditorCommand((e) => {
+		debugger
+		if (e.command === 'editor.action.insertSnippet' && e.text.includes("Table")) {
+		  // 用户执行了 mySnippet 代码片段
+		  console.log('User executed my snippet!');
+		}
+	  });
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(tableSnip);
 }
 
 // This method is called when your extension is deactivated
